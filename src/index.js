8d2be278caa8f3da84a881ff80b74d02f960de06
stiven45ruiz <stiven45ruiz@gmail.com>
2022-04-03 00:24:20 -0500

    Publish package

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const messages = [
+ "Stiven",
+ "Anna",
+ "Diego",
+ "Laura",
+];
+
+const randomMsg = ()=>{
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+};
+
+module.exports = { randomMsg };
